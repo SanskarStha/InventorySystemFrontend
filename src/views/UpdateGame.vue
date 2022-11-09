d8df60b2208e79c70cb356f53b8c2e5b53686a02
Sanskar Shrestha <20216661@life.hkbu.edu.hk>
2022-11-09 18:17:36 +0800

    Update UpdateGame

--- src/views/UpdateGame.vue
+++ src/views/UpdateGame.vue
@@ -3,7 +3,7 @@
     <nav aria-label="breadcrumb">
       <ol class="breadcrumb">
         <li class="breadcrumb-item"><a href="#">Home</a></li>
-        <li class="breadcrumb-item"><a href="#">Books</a></li>
+        <li class="breadcrumb-item"><a href="#">Games</a></li>
         <li class="breadcrumb-item active" aria-current="page">Game Detail</li>
       </ol>
     </nav>
