31315c6e6ae6ea5797bf64b0a22988fa574bae27
Sanskar Shrestha <20216661@life.hkbu.edu.hk>
2022-11-09 17:25:51 +0800

    Create BooksView

---
+++ src/views/BooksView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <BooksPage type="Book" />
+  </div>
+</template>
+
+<script>
+
+import BooksPage from '@/components/BooksPage.vue'
+
+
+export default {
+  name: 'HomeView',
+  components: {
+    BooksPage
+  }
+}
+</script>
