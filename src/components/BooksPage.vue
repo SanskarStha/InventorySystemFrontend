dedf4aeb4c2c139bc8db935b026abd007a93229e
Sanskar Shrestha <20216661@life.hkbu.edu.hk>
2022-11-09 23:49:18 +0800

    update layout to show description in cards

--- src/components/BooksPage.vue
+++ src/components/BooksPage.vue
@@ -27,8 +27,7 @@
         </div>
         <div class="card-body">
           <h5 class="card-title">{{ item.title }}</h5>
-          <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional
-            content. This content is a little bit longer.</p>
+          <p class="card-text">{{ item.description }}</p>
           <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
         </div>
       </router-link>
@@ -41,8 +40,7 @@
         </div>
         <div class="card-body">
           <h5 class="card-title">{{ item.title }}</h5>
-          <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional
-            content. This content is a little bit longer.</p>
+          <p class="card-text">{{ item.description }}</p>
           <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
         </div>
       </router-link>
