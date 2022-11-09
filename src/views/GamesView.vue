f49606effbe1e9a1bab207081ccc76bf2a5bb555
Sanskar Shrestha <20216661@life.hkbu.edu.hk>
2022-11-09 17:31:50 +0800

    create GamesView

---
+++ src/views/GamesView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <GamesPage type="Game" />
+  </div>
+</template>
+
+<script>
+
+import GamesPage from '@/components/GamesPage.vue'
+
+
+export default {
+  name: 'HomeView',
+  components: {
+    GamesPage
+  }
+}
+</script>
